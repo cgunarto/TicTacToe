f58e7234235687e04b6b49ee84b574065c8a5ee9
Andrew Liu <dieseliu321@gmail.com>
2014-10-23 16:18:45 -0700

    Initial Commit

---
+++ TicTacToeChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToeChallenge
+//
+//  Created by Andrew Liu on 10/23/14.
+//  Copyright (c) 2014 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToeChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TicTacToeChallenge
+//
+//  Created by Andrew Liu on 10/23/14.
+//  Copyright (c) 2014 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
